7ad7b02f49a5a776a6834cf53831992d3cfce5a2
anjalisg <anjalisgopinathan@gmail.com>
2018-06-04 11:12:30 -0700

    onSearch method
    MapsActivity has an error with the R variable (what is it?)

--- app/src/main/java/com/example/gopinathana1074/anjalimapsapp/MapsActivity.java
+++ app/src/main/java/com/example/gopinathana1074/anjalimapsapp/MapsActivity.java
@@ -1,11 +1,20 @@
 package com.example.gopinathana1074.anjalimapsapp;
 
 import android.Manifest;
+import android.app.Service;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,9 +23,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private EditText locationSearch;
+    private LocationManager locationManager;
+    private Location myLocation;
+    private static final double LATLNG_RECTANGLE_DIST = (5.0/60.0);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,5 +85,92 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         //Add button to toggle satellite/regular map view
+
+
+        locationSearch = (EditText) findViewById(R.id.editText_SearchAddress);
+    }
+    public void onSearch(View view){
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        //Use LocationManager for user location info
+        LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria =  new Criteria();
+        String provider = service.getBestProvider(criteria, false);
+
+        Log.d("AnjaliMapsApp", "MapsActivity: onSearch: location = " + location);
+        Log.d("AnjaliMapsApp", "MapsActivity: onSearch: provider = " + provider);
+
+        LatLng userLoc = null;
+        try {
+            //Check last known location
+            //Need to specifically list the provider (network or gps)
+            if(locationManager != null){        //if location is known
+                Log.d("AnjaliMapsApp", "MapsActivity: onSearch: LocationManager is not null");
+                if( (myLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER) ) != null ){
+                    userLoc = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                    Log.d("AnjaliMapsApp", "MapsActivity: onSearch: using NETWORK_PROVIDER, current location = (Latitude = " + myLocation.getLatitude()+ ", Longitude = " + myLocation.getLongitude() + ")");
+                    Toast.makeText(this, "UserLoc = (Latitude = " + myLocation.getLatitude()+ ", Longitude = " + myLocation.getLongitude() + ")", Toast.LENGTH_SHORT);
+
+                }
+                else if((myLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)) != null ){
+                    userLoc = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+                    Log.d("AnjaliMapsApp", "MapsActivity: onSearch: using GPS_PROVIDER, current location = (Latitude = " + myLocation.getLatitude()+ ", Longitude = " + myLocation.getLongitude() + ")");
+                    Toast.makeText(this, "UserLoc = (Latitude = " + myLocation.getLatitude()+ ", Longitude = " + myLocation.getLongitude() + ")", Toast.LENGTH_SHORT);
+                }
+                else {
+                    Log.d("AnjaliMapsApp", "MapsActivity: onSearch: myLocation is NULL");
+                }
+            }
+        }
+        catch(SecurityException | IllegalArgumentException e){
+            Log.d("AnjaliMapsApp", "MapsActivity: onSearch: Exception on getLastKnown Location");
+            Toast.makeText(this, "Illegal exception; couldn't find your location!", Toast.LENGTH_SHORT);
+        }
+
+        if(!location.matches("")){
+            Log.d("AnjaliMapsApp", "MapsActivity: onSearch: Location string is not blank");
+
+            //Create geocoder
+            Geocoder geocoder = new Geocoder(this, Locale.US);
+
+            try {
+                //Get a list of addresses
+                addressList = geocoder.getFromLocationName(location, 50,
+                        userLoc.latitude - LATLNG_RECTANGLE_DIST,
+                        userLoc.longitude - LATLNG_RECTANGLE_DIST,
+                        userLoc.latitude + LATLNG_RECTANGLE_DIST,
+                        userLoc.longitude + LATLNG_RECTANGLE_DIST);
+            }
+            catch(IOException e){
+                e.printStackTrace();
+                Log.d("AnjaliMapsApp", "MapsActivity: onSearch: Exception on filling addressList with results from geocoder");
+                Toast.makeText(this, "Illegal exception on filling addressList with results from geocoder!", Toast.LENGTH_SHORT);
+            }
+            if(!addressList.isEmpty()){
+                Log.d("AnjaliMapsApp", "MapsActivity: onSearch: Address list size = " + addressList.size());
+                Toast.makeText(this, ""+addressList.size() + " results found", Toast.LENGTH_SHORT);
+
+                for(int i=0; i<addressList.size(); i++){
+                    Address address = addressList.get(i);
+                    LatLng latlng = new LatLng(  address.getLatitude(), address.getLongitude()  );
+
+                    mMap.addMarker(new MarkerOptions().position(latlng).title("" + i + ": "+ address.getSubThoroughfare()));
+                    Log.d("AnjaliMapsApp", "MapsActivity: onSearch: added marker to searched location");
+                    mMap.animateCamera(CameraUpdateFactory.newLatLng(latlng));
+                }
+
+
+
+            }
+            else{
+                Log.d("AnjaliMapsApp", "MapsActivity: onSearch: NO RESULTS FOUND");
+                Toast.makeText(this, "No results found", Toast.LENGTH_SHORT);
+
+            }
+
+
+
+        }
     }
 }
